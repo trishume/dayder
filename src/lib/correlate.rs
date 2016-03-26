5f47853429aba3a2626a56a1068f6937c60882a8
Marc Mailhot <mmm11105@gmail.com>
2016-03-26 20:55:10 +0000

    Forgot a file

---
+++ src/lib/correlate.rs
@@ -0,0 +1,16 @@
+use lib::btsf::*;
+use lib::stats::*;
+use std::fs::File;
+
+pub fn correlate(data: &BinaryTimeSeries) -> Vec<BinaryTimeSeries>{
+    let mut possibilities = read_btsf_file(&mut File::open("./btsf/mortality.btsf").unwrap());
+
+    for i in 0..possibilities.len() {
+        let (xs, ys) = pairinate(&possibilities[i], &data);
+        possibilities[i].correlation = pearson_correlation_coefficient(&xs, &ys) as f32;
+    }
+
+    possibilities.sort_by(|btsa, btsb| btsa.correlation.partial_cmp(&btsa.correlation).unwrap());
+
+    return possibilities;
+}
