88bbeee93c91af1d61f862da1589e852875a9321
Tristan Hume <tris.hume@gmail.com>
2016-03-26 11:22:58 -0400

    Basic mortality file creation

--- scripts/mortality.rb
+++ scripts/mortality.rb
@@ -15,10 +15,33 @@ end
 # Mortality data
 data = Hash.new { |hash, key| hash[key] = [0]*15 }
 
+unknown_cause = 0
 CSV.foreach("data/extract1.csv", headers: true) do |row|
   cause = row["Cause"]
   cause = codes[cause]
+  if cause == nil
+    unknown_cause += 1
+    next
+  end
   data[cause][row["Year"].to_i - 1999] += row["All ages"].to_i
 end
 
+p unknown_cause
+
 pp data.to_a.sample(100)
+
+# ==== Write out file
+
+f = File.open("btsf/mortality.btsf","w")
+
+# Header
+f.print [1, 4*4, 2*4, data.size].pack("L*")
+
+# Data
+data.each do |name, points|
+  f.print [15, name.length].pack("L*")
+  f.print name
+  points.each_with_index do |d,i|
+    f.print [Time.new(1999+i).to_i, d].pack("le")
+  end
+end
