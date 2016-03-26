3d77fe00c895ad94b578208f27fcd846e473a60e
Tristan Hume <tris.hume@gmail.com>
2016-03-26 11:56:10 -0400

    Also include aggregated 3-character codes

--- scripts/mortality.rb
+++ scripts/mortality.rb
@@ -6,7 +6,7 @@ codes = {}
 
 IO.foreach("data/icd10cm_order_2016.txt") do |line|
   code = line[6..13].rstrip
-  next unless code.length == 4
+  next unless code.length == 4 || code.length == 3
   desc = line[77..-1].rstrip
   # p [code, desc]
   codes[code] = desc
@@ -15,20 +15,20 @@ end
 # Mortality data
 data = Hash.new { |hash, key| hash[key] = [0]*15 }
 
-unknown_cause = 0
-CSV.foreach("data/extract1.csv", headers: true) do |row|
-  cause = row["Cause"]
+def add_data(codes, data, cause, year, count)
   cause = codes[cause]
-  if cause == nil
-    unknown_cause += 1
-    next
-  end
-  data[cause][row["Year"].to_i - 1999] += row["All ages"].to_i
+  return if cause == nil
+  name = "Deaths by #{cause[0].downcase}#{cause[1..-1]}"
+  data[name][year - 1999] += count
 end
 
-p unknown_cause
+CSV.foreach("data/extract1.csv", headers: true) do |row|
+  add_data(codes, data, row["Cause"], row["Year"].to_i, row["All ages"].to_i)
+  add_data(codes, data, row["Cause"][0..-2], row["Year"].to_i, row["All ages"].to_i)
+end
 
 pp data.to_a.sample(100)
+p data.size
 
 # ==== Write out file
 
