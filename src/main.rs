f0b5028a90166df259a5ce76dc01d6a276ae4e2c
Tristan Hume <tris.hume@gmail.com>
2016-04-10 14:56:43 -0400

    Save data between requests

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 extern crate byteorder;
 extern crate stats;
 extern crate iron;
-#[macro_use]
-extern crate router;
 extern crate staticfile;
 extern crate mount;
+extern crate persistent;
 mod lib;
 
 use std::fs::File;
 use std::io::*;
 use std::iter::FromIterator;
-use iron::{Iron, Request, Response, IronResult};
+use iron::{Iron, Request, Response, IronResult, Plugin};
+use iron::typemap::Key;
 use iron::status;
 use iron::headers;
 use iron::mime::Mime;
-use router::{Router};
 use mount::Mount;
 use staticfile::Static;
 use std::path::Path;
+
+#[derive(Copy, Clone)]
+pub struct DataSets;
+impl Key for DataSets { type Value = Vec<lib::btsf::BinaryTimeSeries>; }
 
 fn main() {
     fn request_handler(req: &mut Request) -> IronResult<Response>{
@@ -28,7 +31,7 @@ fn main() {
             return Ok(Response::with((status::BadRequest, "Please send a BTSF file with precisely one chart in it")))
         }
 
-        let data_sets = lib::btsf::read_btsf_file(&mut File::open("./btsf/mortality.btsf").unwrap());
+        let data_sets = req.get::<persistent::Read<DataSets>>().unwrap();
         let result = lib::correlate::correlate(&input_charts[0], &data_sets[..]);
 
         let mut response_data: Vec<u8> = Vec::new();
@@ -43,5 +46,9 @@ fn main() {
         .mount("/", Static::new(Path::new("./public")))
         .mount("/find", request_handler);
 
-    Iron::new(mount).http("localhost:8080").unwrap();
+    let data_sets = lib::btsf::read_btsf_file(&mut File::open("./btsf/mortality.btsf").unwrap());
+    let mut chain = iron::Chain::new(mount);
+    chain.link_before(persistent::Read::<DataSets>::one(data_sets));
+
+    Iron::new(chain).http("localhost:8080").unwrap();
 }
