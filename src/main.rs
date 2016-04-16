24c491434583ae995feadbd4b24148d3367232ec
Tristan Hume <tris.hume@gmail.com>
2016-04-16 15:38:29 -0400

    Switch to appending to vector instead of copying

--- src/main.rs
+++ src/main.rs
@@ -28,9 +28,9 @@ impl Key for CorrCache { type Value = CorrelationCache; }
 
 lazy_static! {
     static ref DATA_SETS: Vec<lib::btsf::BinaryTimeSeries> = {
-        let mut all_data = lib::btsf::read_btsf_file(&mut BufReader::new(File::open("./btsf/fred.btsf").unwrap())).unwrap();
-        let file_2 = lib::btsf::read_btsf_file(&mut File::open("./btsf/canada_gdp.btsf").unwrap()).unwrap();
-        all_data.extend_from_slice(&file_2[..]); // Note: this does a copy, could be more efficient but we only do it at startup
+        let mut all_data = Vec::with_capacity(383550);
+        lib::btsf::read_btsf_file(&mut BufReader::new(File::open("./btsf/fred.btsf").unwrap()), &mut all_data).unwrap();
+        lib::btsf::read_btsf_file(&mut File::open("./btsf/canada_gdp.btsf").unwrap(), &mut all_data).unwrap();
         all_data
     };
 }
@@ -39,9 +39,9 @@ fn main() {
     fn find_handler(req: &mut Request) -> IronResult<Response>{
         let mut buffer: Vec<u8> = Vec::new();
         req.body.read_to_end(&mut buffer).unwrap();
-        let input_charts = match lib::btsf::read_btsf_file(&mut Cursor::new(&mut buffer)) {
-            Ok(charts) => charts,
-            Err(e) => return Err(IronError::new(e, status::BadRequest)),
+        let mut input_charts = Vec::with_capacity(1);
+        if let Err(e) = lib::btsf::read_btsf_file(&mut Cursor::new(&mut buffer), &mut input_charts) {
+            return Err(IronError::new(e, status::BadRequest))
         };
         if input_charts.len() != 1 {
             return Ok(Response::with((status::BadRequest, "Please send a BTSF file with precisely one chart in it")))
